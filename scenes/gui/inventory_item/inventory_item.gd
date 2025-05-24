b3aa4bc0f1924bf0d76d52de5a6b843bf6bd9d87
Stratos <filipek.gracjan@o2.pl>
2025-05-24 13:38:26 +0200

    Prettify the item inventory GUI

--- scenes/gui/inventory_item/inventory_item.gd
+++ scenes/gui/inventory_item/inventory_item.gd
@@ -1,5 +1,5 @@
-extends VBoxContainer
+extends ColorRect
 
 func set_item(item_name: String, item_icon: Texture):
-	$Label.text = item_name
-	$TextureRect.texture = item_icon
+	$MarginContainer/HBox/Label.text = item_name
+	$MarginContainer/HBox/TextureRect.texture = item_icon
